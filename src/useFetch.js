51e0ff878b62a9fbbe7d0391c5a89d914e8b4853
dohvely <chode8703@gmail.com>
2021-11-15 22:19:29 +0900

    #useFetch.js - GET API 호출 로직 구현(공통)

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,19 +1,29 @@
 import { useState, useEffect } from 'react'
 
-function useFetch(url, param) {
+function useFetch({url, param}) {
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState(null)
   const [error, setError] = useState(null)
 
   useEffect(() => {
+
     window
-      .fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
+      .fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        // POST일때만
+        // body: JSON.stringify(param)
+      })
+      .then((response) => response.json())
+      .then((response) => {
+
+        setData(response)
         setLoading(false)
       })
       .catch((error) => {
+
         setError(error)
         setLoading(false)
       })
