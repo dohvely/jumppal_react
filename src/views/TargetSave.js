8382cf4caad1c43d7b4ecd645770b52b2736b381
dohvely <chode8703@gmail.com>
2021-11-24 20:56:42 +0900

    #목표 저장 화면 - input value state 처리(진행중)

--- src/views/TargetSave.js
+++ src/views/TargetSave.js
@@ -1,16 +1,50 @@
+import { useState } from 'react/cjs/react.development'
 import '../assets/jumppal.css'
 
-function TargetSave() {
 
+function TargetSave() {
+  const [start, setStart] = useState('')
+  const [end, setEnd] = useState('')
+
+  // 쩜팔 시작하기 버튼 클릭 이벤트
+  const submit = () => {
+    console.log('submit!!!')
+  }
+
+  // input change 이벤트
+  const changeInputValue = (e, targetStateName) => {
+
+    // console.log(`changeInputValue ::: ${targetStateName} => ${e.target.value}`)
+
+    switch(targetStateName) {
+      case 'start': {
+        setStart(e.target.value)
+        break
+      }
+      case 'end': {
+        setEnd(e.target.value)
+        break
+      }
+    }
+  }
 
   return (
-
     <div className="frm">
       <ul className="ul_def">
         <li className="li_def">설정 기간</li>
         <li className="li_def">
-          <input type="text" className="inp w140 h50" placeholder="YYYYMMDD" /> ~
-          <input type="text" className="inp w140 h50" placeholder="YYYYMMDD" />
+          <input  type="text"
+                  className="inp w140 h50"
+                  placeholder="YYYYMMDD"
+                  value={start}
+                  onChange={(e) => { changeInputValue(e, 'start') }}
+          /> ~
+          <input  type="text"
+                  className="inp w140 h50"
+                  placeholder="YYYYMMDD"
+                  value={end}
+                  onChange={(e) => { changeInputValue(e, 'end') }}
+          />
         </li>
       </ul>
       <ul className="ul_def">
@@ -28,11 +62,10 @@ function TargetSave() {
       </ul>
       
       <div className="btn_wrap">
-        <button className="wp90 h55">쩜팔 시작하기</button>
+        <button className="wp90 h55" onClick={submit}>쩜팔 시작하기</button>
       </div>
     </div>
   )
-
 }
 
 export default TargetSave;
