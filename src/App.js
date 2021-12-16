59f527d3850c320d69770ede737ce91bd58c7943
dohvely <chode8703@gmail.com>
2021-12-16 23:03:59 +0900

    #Github 인증 프로세스 분리(=컴포넌트 분리)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import TargetSave from './views/TargetSave'
+import GithubAuthorize from './views/GithubAuthorize';
 import GithubAuthCode from './views/GithubAuthCode';
 import MainBoard from './views/MainBoard'
 
@@ -9,10 +10,12 @@ function App() {
 
   return (
     <BrowserRouter>
-      {/* 목표 저장 */}
-      <Route path="/target/save" exact component={TargetSave}></Route>
+      {/* Github 인증 */}
+      <Route path="/github/authorize" exact component={GithubAuthorize}></Route>
       {/* Github 인증 코드 수신 */}
       <Route path="/githubauth" exact component={GithubAuthCode}></Route>
+      {/* 목표 저장 */}
+      <Route path="/target/save" exact component={TargetSave}></Route>
       {/* 메인 화면 */}
       <Route path="/" exact component={MainBoard}></Route>
     </BrowserRouter>
---
+++ src/views/GithubAuthorize.js
@@ -0,0 +1,22 @@
+import '../assets/jumppal.css'
+
+
+function GithubAuthorize() {
+
+  const githubClientId = 'd08f513121fec5be6203'
+  const githubCallbackUrl = 'http://localhost:3000/githubauth'
+  const githubLoginUrl = `https://github.com/login/oauth/authorize?client_id=${githubClientId}&redirect_uri=${githubCallbackUrl}`
+
+  return (
+    <div className="frm">
+      <ul className="ul_def">
+        <li className="li_def">Github 연동하기</li>
+        <li className="btn_wrap">
+          <a href={githubLoginUrl}>인증하러가기</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default GithubAuthorize;
