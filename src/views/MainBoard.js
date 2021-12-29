9644e189a55cb690a6c917c15374d7d15cd038ce
dohvely <chode8703@gmail.com>
2021-12-29 22:11:46 +0900

    #메인 - 박스 추가, css 작업

--- src/views/MainBoard.js
+++ src/views/MainBoard.js
@@ -1,8 +1,23 @@
 
+import CommonUtil from '../js/common.util.js'
+
 function MainBoard() {
 
+  let today = new Date()
 
-  return <div>MainBoard!!!</div>
+
+  // 필요한 component들..
+  // 1. gaugebar => slider? Customized sliders
+  // 2. textarea
+  return (
+    <section>
+      <div className="main_box_wrap">
+        <div className="box">{CommonUtil.dateToMMDDKoreanFormat({ date: today })}</div>
+        <div className="box">10/<span>100회</span></div>
+      </div>
+      Yapp!
+    </section>
+  )
 
 }
 
