de880db6734ff3474987c02f58790926c9fcdcb7
dohvely <chode8703@gmail.com>
2022-02-20 21:59:36 +0900

    #메인 - commit 리스트, To-do textarea 추가

--- src/views/MainBoard.js
+++ src/views/MainBoard.js
@@ -1,14 +1,23 @@
 import CommonUtil from '../js/common.util.js'
+import { useState } from 'react/cjs/react.development'
 import Slider from '@mui/material/Slider'
 
 function MainBoard() {
+  const [commitCnt, commitTargetCnt] = useState(0)  // 커밋 수, 목표 커밋 수
+  const commitPer = useState(0) // 커밋 진행률(%)
+  const todoText = useState('') // 나의 To-do 텍스트
 
   let today = new Date()
+
+  // TODO: 최근 commit 리스트 조회
+  // TODO : 나의 To-do 저장
+  const saveTodoText = (todoText) => {
+    console.log('저장하자')
+  }
 
 
   // 필요한 component들..
   // 1. gaugebar => slider? Customized sliders
-  // 2. textarea
   return (
     <section>
       <div className="main_box_wrap">
@@ -22,6 +31,26 @@ function MainBoard() {
           color: 'success.main'
         }}
       ></Slider>
+      <div className="main_box_wrap">
+        <ul>
+          <li>
+            <span>최근 commit.</span>
+          </li>
+          <li>
+            <span>리스트..</span>
+          </li>
+        </ul>
+      </div>
+      <div className="main_box_wrap">
+        <ul>
+          <li>
+            <span>나의 To-do.</span>
+          </li>
+          <li>
+            <textarea value={todoText} onChange={saveTodoText}></textarea>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 
